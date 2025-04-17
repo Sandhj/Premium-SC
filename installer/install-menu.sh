c0d75737ada60ab485559e78ab7041043ed6a790
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-04-17 22:57:07 +0800

    Create install-menu.sh

---
+++ installer/install-menu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+GITHUB = "https://raw.githubusercontent.com/Sandhj/Premium-SC/main/"
+
+wget -q -O /usr/bin/menu "${GITHUB}update/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/menu-ss "${GITHUB}update/menu-ss.sh" && chmod +x /usr/bin/menu-ss
+wget -q -O /usr/bin/menu-vmess "${GITHUB}update/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
+wget -q -O /usr/bin/menu-vless "${GITHUB}update/menu-vless.sh" && chmod +x /usr/bin/menu-vless
+wget -q -O /usr/bin/menu-trojan "${GITHUB}update/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
+wget -q -O /usr/bin/menu-ssh "${GITHUB}update/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
+wget -q -O /usr/bin/menu-set "${GITHUB}update/menu-set.sh" && chmod +x /usr/bin/menu-set
+wget -q -O /usr/bin/menu-backup "${GITHUB}update/menu-backup.sh" && chmod +x /usr/bin/menu-backup
+wget -q -O /usr/bin/menu-ip "${GITHUB}update/menu-ip.sh" && chmod +x /usr/bin/menu-ip
+wget -q -O /usr/bin/menu-tor "${GITHUB}update/menu-tor.sh" && chmod +x /usr/bin/menu-tor
+wget -q -O /usr/bin/auto-reboot "${GITHUB}update/auto-reboot.sh" && chmod +x /usr/bin/auto-reboot
+wget -q -O /usr/bin/menu-tcp "${GITHUB}update/menu-tcp.sh" && chmod +x /usr/bin/menu-tcp
+wget -q -O /usr/bin/rebootvps "${GITHUB}corn/rebootvps.sh" && chmod +x /usr/bin/rebootvps
+wget -q -O /usr/bin/info "${GITHUB}update/info.sh" && chmod +x /usr/bin/info
+wget -q -O /usr/bin/mbandwith "${GITHUB}update/menu-bandwith.sh" && chmod +x /usr/bin/mbandwith
