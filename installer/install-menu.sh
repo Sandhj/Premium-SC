f09c2710b907d422135a52f778670fd18e8a062a
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-04-17 23:12:53 +0800

    Update install-menu.sh

--- installer/install-menu.sh
+++ installer/install-menu.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 GITHUB = "https://raw.githubusercontent.com/Sandhj/Premium-SC/main/"
 
-wget -q -O /usr/bin/menu "${GITHUB}update/menu.sh" && chmod +x /usr/bin/menu
-wget -q -O /usr/bin/menu-ss "${GITHUB}update/menu-ss.sh" && chmod +x /usr/bin/menu-ss
-wget -q -O /usr/bin/menu-vmess "${GITHUB}update/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
-wget -q -O /usr/bin/menu-vless "${GITHUB}update/menu-vless.sh" && chmod +x /usr/bin/menu-vless
-wget -q -O /usr/bin/menu-trojan "${GITHUB}update/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
-wget -q -O /usr/bin/menu-ssh "${GITHUB}update/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
-wget -q -O /usr/bin/menu-set "${GITHUB}update/menu-set.sh" && chmod +x /usr/bin/menu-set
-wget -q -O /usr/bin/menu-backup "${GITHUB}update/menu-backup.sh" && chmod +x /usr/bin/menu-backup
-wget -q -O /usr/bin/menu-ip "${GITHUB}update/menu-ip.sh" && chmod +x /usr/bin/menu-ip
-wget -q -O /usr/bin/menu-tor "${GITHUB}update/menu-tor.sh" && chmod +x /usr/bin/menu-tor
-wget -q -O /usr/bin/auto-reboot "${GITHUB}update/auto-reboot.sh" && chmod +x /usr/bin/auto-reboot
-wget -q -O /usr/bin/menu-tcp "${GITHUB}update/menu-tcp.sh" && chmod +x /usr/bin/menu-tcp
-wget -q -O /usr/bin/rebootvps "${GITHUB}corn/rebootvps.sh" && chmod +x /usr/bin/rebootvps
-wget -q -O /usr/bin/info "${GITHUB}update/info.sh" && chmod +x /usr/bin/info
-wget -q -O /usr/bin/mbandwith "${GITHUB}update/menu-bandwith.sh" && chmod +x /usr/bin/mbandwith
+wget -q -O /usr/bin/menu "${GITHUB}menu/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/menu-ss "${GITHUB}menu/menu-ss.sh" && chmod +x /usr/bin/menu-ss
+wget -q -O /usr/bin/menu-vmess "${GITHUB}menu/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
+wget -q -O /usr/bin/menu-vless "${GITHUB}menu/menu-vless.sh" && chmod +x /usr/bin/menu-vless
+wget -q -O /usr/bin/menu-trojan "${GITHUB}menu/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
+wget -q -O /usr/bin/menu-ssh "${GITHUB}menu/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
+wget -q -O /usr/bin/menu-set "${GITHUB}menu/menu-set.sh" && chmod +x /usr/bin/menu-set
+wget -q -O /usr/bin/menu-backup "${GITHUB}menu/menu-backup.sh" && chmod +x /usr/bin/menu-backup
+wget -q -O /usr/bin/menu-ip "${GITHUB}menu/menu-ip.sh" && chmod +x /usr/bin/menu-ip
+wget -q -O /usr/bin/menu-tor "${GITHUB}menu/menu-tor.sh" && chmod +x /usr/bin/menu-tor
+wget -q -O /usr/bin/auto-reboot "${GITHUB}menu/auto-reboot.sh" && chmod +x /usr/bin/auto-reboot
+wget -q -O /usr/bin/menu-tcp "${GITHUB}menu/menu-tcp.sh" && chmod +x /usr/bin/menu-tcp
+wget -q -O /usr/bin/info "${GITHUB}menu/info.sh" && chmod +x /usr/bin/info
+wget -q -O /usr/bin/mbandwith "${GITHUB}menu/menu-bandwith.sh" && chmod +x /usr/bin/mbandwith
