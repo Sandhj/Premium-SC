e37f6796aea017fb3fea4d470e09137b9ba5fb3f
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-04-20 22:18:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,45 +1,73 @@
 #!/bin/bash
 
-# SET DOMAIN
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo -e " Masukkan Domain Yang Sudah Du pointing Ke IP VPS"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-read -p "Enter :" domen
-mkdir -p /etc/xray/
-echo $domen > /root/domain
-echo "$domen" > /etc/xray/domain
+echo -e "╭══════════════════════════════════════════╮"
+echo -e "│             Please Insert Your Domain"
+echo -e "╰══════════════════════════════════════════╯"
+read -p "   Your Domain : " dnss
+mkdir -p /etc/xray
+touch /etc/xray/domain
+echo "$dnss" > /root/domain
+echo "$dnss" > /etc/xray/domain
+echo "IP=$dnss" > /var/lib/ipvps.conf
 
-# UBAH TIME ZONE
-ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
-# DISABLE IPV6
-sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
-sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
+echo -e "╭══════════════════════════════════════════╮"
+echo -e "│             INSTALL SSH/OVPN
+echo -e "╰══════════════════════════════════════════╯"
+wget https://raw.githubusercontent.com/king-vpn/autoscript-vip/main/install/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+clear
 
-# INSTALL XRAY
-wget https://raw.githubusercontent.com/Sandhj/Premium-SC/main/installer/install-xray.sh && chmod +x install-xray.sh && ./install-xray.sh
 
-# INSTALL MENU
-wget https://raw.githubusercontent.com/Paper890/mysc/main/installer/install-menu.sh && chmod +x install-menu.sh && ./install-menu.sh
+echo -e "╭══════════════════════════════════════════╮"
+echo -e "│             INSTALL XRAY
+echo -e "╰══════════════════════════════════════════╯"
+wget https://raw.githubusercontent.com/king-vpn/autoscript-vip/main/install/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+clear
 
-# PASANG DEFAULT MENU
+
+echo -e "╭══════════════════════════════════════════╮"
+echo -e "│             INSTALL WEBSOCKET
+echo -e "╰══════════════════════════════════════════╯"
+wget https://raw.githubusercontent.com/king-vpn/autoscript-vip/main/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
+clear
+
+
+echo -e "╭══════════════════════════════════════════╮"
+echo -e "│             INSTALL OHP
+echo -e "╰══════════════════════════════════════════╯"
+wget https://raw.githubusercontent.com/king-vpn/autoscript-vip/main/sshws/ohp.sh && chmod +x ohp.sh && ./ohp.sh
+clear
+
+
+echo -e "╭══════════════════════════════════════════╮"
+echo -e "│             INSTALL MENU
+echo -e "╰══════════════════════════════════════════╯"
+wget https://raw.githubusercontent.com/king-vpn/autoscript-vip/main/update.sh && chmod +x update.sh && ./update.sh
+clear
+
+
 cat> /root/.profile << END
-# ~/.profile: executed by Bourne-compatible login shells.
-
 if [ "$BASH" ]; then
-  if [ -f ~/.bashrc ]; then
-    . ~/.bashrc
-  fi
+if [ -f ~/.bashrc ]; then
+. ~/.bashrc
 fi
-
+fi
 mesg n || true
 clear
 menu
 END
 chmod 644 /root/.profile
 
-echo " DONE SETUP"
 
-# HAPUS FILE INSTALLER
 cd
-rm *
+rm -rf *
+
+echo -e "╭══════════════════════════════════════════╮"
+echo -e "│             ISNTALL SUCCESSFULLY 
+echo -e "╰══════════════════════════════════════════╯"
+echo -e " Do you want to reboot now ? (y/n)? "; read answer
+if [ "$answer" == "${answer#[Yy]}" ] ;then
+exit 0
+else
+reboot
+fi
